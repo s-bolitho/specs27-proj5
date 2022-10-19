b0baf708e4b7f42f9c815d24f106ca34af4ac657
s-bolitho <sambolitho4@gmail.com>
2022-10-18 22:35:00 -0500

    initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+
+export default configureStore({
+    reducer: { potentialCountries: potentialCountriesReducer },
+});
